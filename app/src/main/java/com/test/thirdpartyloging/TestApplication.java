52ba2e3e46cb1ab7321c4e5ad038fa6011a6ede2
席院 <s64j673@icloud.com>
2018-10-23 11:50:35 +0800

    up

--- app/src/main/java/com/test/thirdpartyloging/TestApplication.java
+++ app/src/main/java/com/test/thirdpartyloging/TestApplication.java
@@ -2,8 +2,6 @@ package com.test.thirdpartyloging;
 
 import android.app.Application;
 
-import com.umeng.socialize.PlatformConfig;
-
 /**
  * Created by chencha on 16/8/23.
  */
@@ -15,12 +13,12 @@ public class TestApplication extends Application {
         super.onCreate();
     }
 
-    {
-        //微信 appid appsecret
-        PlatformConfig.setWeixin("#####", "#####");
-        //  新浪微博 appid appsecret
-        PlatformConfig.setSinaWeibo("####", "####");
-        //qq appid appsecret
-        PlatformConfig.setQQZone("####", "#####");
-    }
+//    {
+//        //微信 appid appsecret
+//        PlatformConfig.setWeixin("#####", "#####");
+//        //  新浪微博 appid appsecret
+//        PlatformConfig.setSinaWeibo("####", "####");
+//        //qq appid appsecret
+//        PlatformConfig.setQQZone("####", "#####");
+//    }
 }
